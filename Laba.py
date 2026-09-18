b107df9d0309fdcb1b4aaeddcb6cf80bcba7c53c
MelnuchykYaroslav <melnuchyk.yaroslav2010@gmail.com>
2026-09-18 08:00:01 +0300

    Add files via upload

---
+++ Laba.py
@@ -0,0 +1,51 @@
+print("Hello World")
+int_var = 42
+float_var = 3.14
+str_var = "перша лаба"
+bool_var = True
+list_var = [1, 2, 3]
+tuple_var = (10, 20, 30)
+dict_var = {"name": "Ivan", "group": "IST-21", "year": 2026}
+set_var = {1, 2, 3}
+print("зміні та їх типи")
+print(int_var, type(int_var))
+print(float_var, type(float_var))
+print(str_var, type(str_var))
+print(bool_var, type(bool_var))
+print(list_var, type(list_var))
+print(tuple_var, type(tuple_var))
+print(dict_var, type(dict_var))
+print(set_var, type(set_var))
+print("арифметичні оператори")
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
+print(a > 5 and b < 5)
+print(a > 5 or b > 5)
+print(not a > 5)
+print("оператори присвоєння")
+c = a
+c += 5
+print(c)
+c -= 3
+print(c)
+c *= 2
+print(c)
+c /= 4
+print(c)
+
